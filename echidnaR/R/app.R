d0a4e07a25dee915683aee5d5d6fef04352c64ed
Carson Sievert <cpsievert1@gmail.com>
2016-04-22 09:22:49 +1000

    packagify app

--- echidnaR/R/app.R
+++ echidnaR/R/app.R
@@ -1,7 +1,7 @@
 #' Shiny app for exploring census and electorate data
 #' 
 #' @import shiny
-#' @import ggplot2
+#' @import plotly
 #' @importFrom tidyr gather
 #' @export
 #' @examples \dontrun{
@@ -10,6 +10,7 @@
 
 launchApp <- function() {
   data("abs2011", package = "echidnaR")
+  data("aec2013", package = "echidnaR")
   longAbs <- tidyr::gather(abs2011, variable, value, -ID, -Name, -State)
   longAbs$value <- as.numeric(longAbs$value)
   longAbs <- longAbs[!is.na(longAbs$value),]
@@ -22,10 +23,7 @@ launchApp <- function() {
   )
   electorates <- unique(abs2011$ELECT_DIV)
   ui <- fluidPage(
-    selectizeInput(
-      "electorates", "Select electorate(s):", choices = electorates, 
-      selected = c("Brisbane", "Melbourne"), multiple = TRUE
-    ),
+    plotlyOutput("map"),
     fluidRow(
       column(
         width = 6,
@@ -39,10 +37,31 @@ launchApp <- function() {
   )
   
   server <- function(input, output) {
+    
+   output$map <- renderPlotly({
+     p <- ggplot(data = nat_map, 
+                 aes(long, lat, group = group, key = ELECT_DIV,
+                     text = ELECT_DIV)) + 
+       geom_polygon() + ggthemes::theme_map()
+     
+     l <- plotly_build(ggplotly(p, tooltip = "text"))
+     #l$data[[1]]$type <- "scattergl"
+     l$layout$height <- 400
+     l$layout$width <- 400
+     l
+     
+     # I reckon it'd be better to work with the spaital polygon
+     #leaflet() %>%
+     #  addPolygons(nat_map$long, nat_map$lat, group = nat_map$group)
+     
+     
+   })
+    
     output$ages <- renderPlot({
       p <- ggplot(ages, aes(value)) + geom_density()
-      if (!is.null(input$electorates)) {
-        d <- ages[ages$Name %in% input$electorates, ]
+      ed <- event_data("plotly_click")
+      if (!is.null(ed)) {
+        d <- ages[ages$Name %in% ed$key, ]
         p <- p + geom_vline(data = d, aes(xintercept = value, color = Name))
       }
       p + 
@@ -52,8 +71,9 @@ launchApp <- function() {
     
     output$densities <- renderPlot({
       p <- ggplot(other, aes(value)) + geom_density()
-      if (!is.null(input$electorates)) {
-        d <- other[other$Name %in% input$electorates, ]
+      ed <- event_data("plotly_click")
+      if (!is.null(ed)) {
+        d <- other[other$Name %in% ed$key, ]
         p <- p + geom_vline(data = d, aes(xintercept = value, color = Name))
       }
       p + 
