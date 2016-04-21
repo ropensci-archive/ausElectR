2004547bbca245c3329d7eef0a5142e456023c53
Rob J Hyndman <robjhyndman@gmail.com>
2016-04-21 15:22:10 +1000

    Fixed names of electorates to match AEC

--- ReadABS.R
+++ ReadABS.R
@@ -29,6 +29,7 @@ rm(bfiles,newcols,bnew,i)
 # Create a new data frame with a subset of the variables
 abs2011 <- data.frame(ID = substr(abs2011all$region_id,4,6))
 abs2011$Name <- abs2011all$Name
+abs2011$Name[abs2011$Name=="Mcewen"] <- "McEwen"
 abs2011$State <- abs2011all$State
 abs2011$Population <- abs2011all$Tot_P_P
 abs2011$Area <- abs2011all$`Area sqkm`
