3a021afab641bbaf3c668087ed302339bcb1b71a
Ben Marwick <benmarwick@gmail.com>
2016-04-22 12:41:50 +1000

    check party votes per electorate with Anthony

--- AECdata/tidying_and_testing_AEC_data.R
+++ AECdata/tidying_and_testing_AEC_data.R
@@ -145,8 +145,19 @@ load("echidnaR/data/aec2013.rda")
 
 ################################################################
 ## Overall results for first preferences -----------------------
+
+# by party by electorate
+aec2013 %>% 
+  select(DivisionID.x, PartyNm, OrdinaryVotes) %>% 
+  group_by(PartyNm, DivisionID.x) %>% 
+  summarise(total_votes = sum(OrdinaryVotes)) %>% 
+  ungroup() %>%
+  arrange(DivisionID.x, total_votes)
+
+
+
 # by party
-election_results_df_loc_no_fac_no_dup %>% 
+aec2013 %>% 
   select(PartyNm, OrdinaryVotes) %>% 
   group_by(PartyNm) %>% 
   summarise(total_votes = sum(OrdinaryVotes)) %>% 
@@ -167,7 +178,7 @@ aes_first_pref %>%
 ################################################################
 # winner for each electorate ----------------------------------
 
-election_results_df_loc_no_fac_no_dup %>% 
+aec2013 %>% 
   group_by(DivisionID.x) %>% 
   select(DivisionID.x, DivisionNm.x, StateAb, GivenNm, Surname, PartyNm, Elected) %>% 
   filter(Elected == "Y") %>% 
@@ -188,7 +199,7 @@ aes_winners %>%
 
 ################################################################
 # Comparing party and candidate votes of several parties -------
-proportions <- election_results_df_loc_no_fac_no_dup %>%
+proportions <- aec2013 %>%
   group_by(DivisionID.x) %>%
   summarise(Prop_Labour = sum(OrdinaryVotes[PartyNm == "Australian Labor Party"]) / sum(OrdinaryVotes),
             Prop_Liberal = sum(OrdinaryVotes[PartyNm == "Liberal"]) / sum(OrdinaryVotes),
@@ -218,7 +229,7 @@ names(polling_place_location)
 
 
 # plot electorates to show proportion of labor votes
-election_results_df_loc_no_fac_no_dup %>%
+aec2013 %>%
   # compute proportion of votes for labor by
   group_by(PollingPlaceID) %>% 
   summarise(ProportionLabour = sum(OrdinaryVotes[PartyNm == "Australian Labor Party"]) / sum(OrdinaryVotes)) %>% 
@@ -238,7 +249,7 @@ election_results_df_loc_no_fac_no_dup %>%
 # Rolling up results to electorate
 
 # watch out for zeros...
-election_results_df_loc_no_fac_no_dup %>% 
+aec2013 %>% 
   group_by(PollingPlaceID) %>%
   summarise(TotalVotes = sum(OrdinaryVotes))   %>% 
   filter(TotalVotes == 0) %>% 
@@ -248,7 +259,7 @@ election_results_df_loc_no_fac_no_dup %>%
 
 # exclude polling  places with no votes
 library(scales)
-p <- election_results_df_loc_no_fac_no_dup %>%
+p <- aec2013 %>%
   group_by(DivisionNm.x) %>%
   summarise(
     TotalVotes = sum(OrdinaryVotes),
@@ -290,7 +301,7 @@ parties_of_interest <- c("ALP", "GRN", "LP", "NP", "CLP", "LNQ")
 
 electorate_level_formal_vote_counts_by_major_party <- 
   # formal vote
-  election_results_df_loc_no_fac_no_dup %>% 
+  aec2013 %>% 
   mutate(formal = BallotPosition != 999) %>% 
   group_by(DivisionNm.x, PartyAb) %>% 
   summarize(total_formal = sum(OrdinaryVotes[formal], na.rm=TRUE),
